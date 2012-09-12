000158d8c7486b31e338362d9a2693aca03ccc51
Daniel Pakuschewski <daniel@mobvox.com.br>
2012-09-12 19:41:47 -0300

    Partial add composer install

--- lib/mina-composer/composer.rb
+++ lib/mina-composer/composer.rb
@@ -1,3 +1,17 @@
-namespace :composer do  
-  
+namespace :composer do
+
+  task :install do
+    install_path = deploy_to + '/' + shared_path
+    queue %{
+      echo "-----> Installing Composer." && (
+        #{echo_cmd %{cd #{install_path}}} &&
+        #{echo_cmd %{curl -s https://getcomposer.org/installer | php}} &&
+        #{echo_cmd %{cd #{deploy_to}}} &&
+        echo "------> Done."
+      ) || (
+        echo "! ERROR: Install composer failed."
+        echo "! Ensure that the path '#{deploy_to}' is accessible to the SSH user."
+      )
+    }
+  end
 end
